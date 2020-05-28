0bba0d68cd5936dc80c9131d6d27132645e18fa7
CutRay <zeron9421@gmail.com>
2020-05-29 05:09:06 +0900

    2章まで完成

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title = 'Flutterサンプル';
+  final String message = 'Hello';
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter Demo',
+        home: new MyHomePage(
+          title: this.title,
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({this.title}) : super();
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class Data {
+  int _price;
+  String _name;
+  Data(this._name, this._price) : super();
+
+  @override
+  String toString() {
+    return _name + _price.toString() + '円';
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final _data = [
+    Data('Apple', 200),
+    Data('Orange', 150),
+    Data('Peach', 300)
+  ];
+  Data _item;
+  @override
+  void initState() {
+    super.initState();
+    _item = _data[0];
+  }
+
+  void _setMessage() {
+    setState(() {
+      _item = (_data..shuffle()).first;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Text(_item.toString(), style: TextStyle(fontSize: 32.0)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _setMessage,
+          tooltip: 'set message',
+          child: Icon(Icons.star),
+        ));
+  }
+}
