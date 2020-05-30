97c2d7dec9b7b8aa1cc09b32b08896ffc5467968
CutRay <zeron9421@gmail.com>
2020-05-31 03:00:31 +0900

    FlatButton完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  debugPaintSizeEnabled = true;
   runApp(new MyApp());
 }
 
@@ -27,43 +29,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _message;
+  static var _janken = <String>['グー', 'チョキ', 'パー'];
+  @override
+  void initState() {
+    _message = 'ok';
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text('App Name'),
-      ),
-      body: Center(
-          child: Card(
-        margin: EdgeInsets.all(50.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Hello',
-              style: TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Roboto'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-            ),
-            Text(
-              'This is Card Layout' * 5,
-              style: TextStyle(
-                  fontSize: 24.0,
-                  color: const Color(0xFF0000FF),
-                  fontWeight: FontWeight.w200,
-                  fontFamily: 'Roboto'),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('App Name'),
         ),
-      )),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.android), onPressed: fabPressed),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(_message,
+                      style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 32.0))),
+              FlatButton(
+                onPressed: buttonPressed,
+                color: Colors.black12,
+                child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Icon(Icons.android, size: 50.0)),
+              ),
+            ],
+          ),
+        ));
   }
 
-  void fabPressed() {}
+  void buttonPressed() {
+    setState(() {
+      _message = (_janken..shuffle()).first;
+    });
+  }
 }
