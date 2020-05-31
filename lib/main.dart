71b3101b4406efbfd05cbfef55f69dbd0567fe17
CutRay <zeron9421@gmail.com>
2020-06-01 00:35:00 +0900

    DrawImage

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
+
+import 'dart:typed_data';
+import 'dart:async';
 import 'dart:ui' as ui;
 
 void main() {
@@ -54,10 +58,27 @@ class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
 }
 
 class _MyRenderBox extends RenderBox {
+  ui.Image _img;
+
   @override
   bool hitTest(HitTestResult result, {@required Offset position}) {
     return true;
   }
+
+  _MyRenderBox() {
+    loadAssetImage('img.jpg');
+  }
+
+  loadAssetImage(String fname) => rootBundle.load("assets/$fname").then((bd) {
+        Uint8List u8lst = Uint8List.view(bd.buffer);
+        ui.instantiateImageCodec(u8lst).then((codec) {
+          codec.getNextFrame().then((frameInfo) {
+            _img = frameInfo.image;
+            markNeedsPaint();
+            print("_img created: $_img");
+          });
+        });
+      });
 
   @override
   void paint(PaintingContext context, Offset offset) {
@@ -65,20 +86,13 @@ class _MyRenderBox extends RenderBox {
     int dx = offset.dx.toInt();
     int dy = offset.dy.toInt();
 
-    ui.ParagraphBuilder builder = ui.ParagraphBuilder(
-      ui.ParagraphStyle(textDirection: TextDirection.ltr),
-    )
-      ..pushStyle(ui.TextStyle(color: Colors.red, fontSize: 48.0))
-      ..addText('Hello! ')
-      ..pushStyle(ui.TextStyle(color: Colors.blue[700], fontSize: 30.0))
-      ..addText('This is a sample of paragraph text. ')
-      ..pushStyle(ui.TextStyle(color: Colors.blue[200], fontSize: 30.0))
-      ..addText('You can draw MULTI-FONT text!');
-
-    ui.Paragraph paragraph = builder.build()
-      ..layout(ui.ParagraphConstraints(width: 300.0));
-
+    Paint p = Paint();
     Offset off = Offset(dx + 50.0, dy + 50.0);
-    c.drawParagraph(paragraph, off);
+    if (_img != null) {
+      c.drawImage(_img, off, p);
+      print('draw _img.');
+    } else {
+      print('_img is null.');
+    }
   }
 }
